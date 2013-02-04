59cc065b5322e9bc19458fccd5c2f4fb9ebae4bf
Jean-Philippe Boily <j@jipi.ca>
2013-02-03 21:54:56 -0500

    initial commit for rspec-dummy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecDummy::Application.config.secret_token = '714900d961fcd965f3b50e86d186c9285cf3a04f6f0ff1f5ebdabeceb41b623d0e51e44532c84bee599d3b6059a9330a147561921e12b521768bfd759db24749'
---
+++ spec/models/meh_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+describe 'meh' do
+  it { 1.should eq(2) }
+end
